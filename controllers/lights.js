d6700b6e7735722d4047d6640422aaa2c056ef7a
Towa Quimbayo <towacurtis@hotmail.com>
2025-03-24 16:53:30 -0700

    fixed light controller

--- controllers/lights.js
+++ controllers/lights.js
@@ -1,7 +1,13 @@
 const { apiClient } = require("../utils/helpers");
 
 const controlLights = async (req, res) => {
-  const { action, deviceName } = req.body;
+  const {
+    action,
+    deviceName,
+    brightness = "", // 0-255
+    color = "", // [255, 255, 255]
+    effect = "", // "effect_colorloop"
+  } = req.body;
 
   if (!action || !deviceName) {
     console.error("Missing parameters.");
@@ -23,10 +29,18 @@ const controlLights = async (req, res) => {
       return res.status(400).json({ success: false, error: "Invalid action." });
   }
 
+  // prepare the payload
+  const payload = { entity_id: `light.${deviceName}` };
+
+  // if action is "on", add brightness, color and effect to the payload
+  if (action === "on") {
+    if (brightness) payload.brightness = brightness;
+    if (color) payload.color = color;
+    if (effect) payload.effect = effect;
+  }
+
   try {
-    const response = await apiClient.post(`services/light/${service}`, {
-      entity_id: `light.${deviceName}`,
-    });
+    const response = await apiClient.post(`services/light/${service}`, payload);
 
     if (response.status !== 200) {
       console.error(`Error: ${response.data}`);
